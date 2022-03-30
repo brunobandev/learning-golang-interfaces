8583c4dc8913e22520928816925b8c2e93821f12
Bruno Bandeira <brunoban.dev@gmail.com>
2022-03-30 19:04:58 +0100

    implementing the first interface

--- main.go
+++ main.go
@@ -2,24 +2,24 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
 	eb := englishBot{}
-	// sb := spanishBot{}
+	sb := spanishBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
-
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting())
-// }
 
 func (englishBot) getGreeting() string {
 	return "Hi There"
